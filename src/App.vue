92176475d49ac1d1c038e455299cc3ee92d35ce1
muhammadBilal924 <mbilal60924@gmail.com>
2022-02-17 17:02:51 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <DataBind />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import DataBind from './components/DataBind.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    DataBind
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/DataBind.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+    <h1>Data Binding in vue js</h1>
+    <h2 style="white-space:pre-line">{{ message}}</h2>
+    <textarea type="text" v-model="message" />
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"DataBind",
+        data(){
+            return{
+                message:"Hi"
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
